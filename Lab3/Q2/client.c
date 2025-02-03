2a465251aea6f8c30bc7b56c5cf9f1c9623761e8
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2025-02-03 16:04:30 +0530

    Update client.c

--- Lab3/Q2/client.c
+++ Lab3/Q2/client.c
@@ -12,9 +12,9 @@
 int main()
 {
     int sockfd, retval;
+    int sendbytes;
     socklen_t actuallen;
     struct sockaddr_in serveraddr, clientaddr;
-    int recedbytes, sendbytes;
     char buff[max];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,54 +25,31 @@ int main()
     }
 
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(3388);
+    serveraddr.sin_port = htons(3223);
     serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     retval = connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
     if (retval == -1)
     {
-        printf("Error Connecting to the port...");
+        printf("Error Connecting to server..");
         exit(0);
     }
 
-    actuallen = sizeof(serveraddr);
-    pid_t p = fork();
-    if (p == 0)
+    while (1)
     {
-        printf("Child process: %d", getpid());
-        while (1)
+        memset(buff, '\0', sizeof(buff));
+        printf("Enter String: ");
+        fgets(buff, sizeof(buff), stdin);
+        if (strcmp(buff, "stop") == 0)
         {
-            memset(buff, '\0', sizeof(buff));
-            printf("Enter Message: ");
-            fgets(buff, sizeof(buff), stdin);
-            if (strcmp(buff, "Halt") == 0)
-            {
-                printf("Closing server....");
-                exit(0);
-            }
-            sendbytes = send(sockfd, buff, sizeof(buff), 0);
-            if (sendbytes == -1)
-            {
-                printf("Error Sending Message");
-                break;
-            }
+            printf("Closing Connection..");
+            exit(0);
         }
-    }
-    else if (p > 0)
-    {
-        printf("Parent process: %d", getppid());
-        printf("\n");
-        while (1)
+        sendbytes = send(sockfd, buff, sizeof(buff), 0);
+        if (sendbytes == -1)
         {
-            memset(buff, '\0', sizeof(buff));
-            recedbytes = recv(sockfd, buff, sizeof(buff), 0);
-            if (recedbytes == -1)
-            {
-                printf("Error REceiving message");
-                break;
-            }
-            printf("%s", buff);
-            printf("\n");
+            printf("Error Sending message");
+            break;
         }
     }
     exit(0);
